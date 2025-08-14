8003bbf21e20ec095dd417f60e8144dedc646266
sakshi sharma <sakshisharma57268@gmail.com>
2025-08-14 15:55:21 +0530

    Updated project with latest changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import '../pages/home.css';
+import html from '../assets/html.webp';
+import cs from '../assets/cs.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import express from '../assets/express.png';
+import canva from '../assets/canva.png';
+
+
+
 
 const Home = () => {
   return (
-<section className='container'>
-<div className='home-content'>
-  <h1>I'm <span>Sakshi Sharma</span></h1>
-  
-   <p>A passionate web developer creating amazing digital experiences.</p>
+    <section className='container'>
+      
+      {/* Bubble images */}
+      <div className='bubbles'>
+        <img src={html} alt="bubble" />
+        <img src={cs} alt="bubble" />
+        <img src={javascript} alt="bubble" />
+        <img src={node} alt="bubble" />
+        <img src={express} alt="bubble" />
+        <img src={canva} alt="bubble" />
+        
+      </div>
 
- 
-  <div className='buttons'>
- 
-    <a href='https://www.canva.com/design/DAGvwxWUCO0/c_52YD0ZYgj5CHuyYdNPBw/view?utm_content=DAGvwxWUCO0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h9bed404f79' download className='btn'>Download Resume</a>
-    <a href='http://localhost:3000/contact' className='btn'>Contact me</a>   
-    </div>
+      {/* Main content */}
+      <div className='home-content'>
+        <h1>I'm <span>Sakshi Sharma</span></h1>
+        <p>A passionate web developer creating amazing digital experiences.</p>
 
-    <div className='icons'>
-      <a href='https://github.com/sakshi282002'><i class="fa-brands fa-github"></i></a>
-      <a href='https://www.linkedin.com/feed/'><i class="fa-brands fa-linkedin"></i></a>
-      <a href='https://www.linkedin.com/feed/'><i class="fa-brands fa-facebook"></i></a>
+        <div className='buttons'>
+          <a href='https://www.canva.com/design/DAGvwxWUCO0/c_52YD0ZYgj5CHuyYdNPBw/view' download className='btn'>Download Resume</a>
+          <a href='/contact' className='btn'>Contact me</a>   
+        </div>
 
-    </div>
+        <div className='icons'>
+          <a href='https://github.com/sakshi282002'><i className="fa-brands fa-github"></i></a>
+          <a href='https://www.linkedin.com/feed/'><i className="fa-brands fa-linkedin"></i></a>
+          <a href='https://www.linkedin.com/feed/'><i className="fa-brands fa-facebook"></i></a>
+        </div>
+      </div>
 
-</div>
-
-</section>
+    </section>
   )
 }
 
